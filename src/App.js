5c454c4f70dfd50be13bc791fe5aebb6d866c110
Frank712 <franksuriel_7@hotmail.com>
2020-06-19 14:21:13 -0500

    ALL Feautures PAW App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { loadCSS } from 'fg-loadcss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import AppBar from './components/shared/AppBar'
+import Login from './components/auth/Login';
+import HomePage from './components/home/HomePage'
+import AlumnoPage from './components/alumno/AlumnoPage'
+import AgentePage from './components/agente/AgentePage'
+import UserPage from './components/user/UserPage'
+import ETSPage from './components/ets/ETSPage'
 
 function App() {
+  const [auth, setAuth] = React.useState(true);
+
+  useEffect(() => {
+    const node = loadCSS(
+      'https://use.fontawesome.com/releases/v5.12.0/css/all.css',
+      document.querySelector('#font-awesome-css'),
+    );
+
+    return () => {
+      node.parentNode.removeChild(node);
+    };
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppBar auth={auth} setAuth={setAuth} />
+      <Switch>
+        <Route exact path='/home' component={HomePage} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/alumnos' component={AlumnoPage} />
+        <Route exact path='/agentes' component={AgentePage} />
+        <Route exact path='/users' component={UserPage} />
+        <Route exact path='/ets' component={ETSPage} />
+      </Switch>
+    </Router>
   );
 }
 
